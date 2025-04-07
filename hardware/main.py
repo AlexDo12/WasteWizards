9abdd05880a670ee70dfd35b30d32681fe2a6edd
Kousuke Todd <mrkotodd@gmail.com>
2025-04-07 13:57:47 -0500

    usonic class made, added functionality to main

--- hardware/main.py
+++ hardware/main.py
@@ -1,4 +1,5 @@
 # import statements
+from ultrasonic import Ultrasonic
 
 def dist():
     distance = 0
@@ -14,7 +15,24 @@ def scanImg():
     classification = ""
     return classification
 
-if __name__ == "main":
+ULTRASONIC_ECHO_PIN = 23
+ULTRASONIC_TRIGGER_PIN = 24
+
+# used for averaging bin capacity
+usonic_distances = []
+
+if __name__ == "__main__":
+    usonic = Ultrasonic(ULTRASONIC_ECHO_PIN, ULTRASONIC_TRIGGER_PIN, 1)
+    try:
+        conn = usonic.connect_db()
+        usonic.setup()
+
+        # ... after trapdoor opens...MAKE SURE TO SET BIN NUMBER ON USONIC
+        # report bin capacity
+        usonic.bin = 1 # CHANGE THIS ACCORDINGLY
+        usonic.update_fill_level(conn, usonic.run())
+    except:
+        print("Error in main")
     
 
 
@@ -22,7 +40,7 @@ if __name__ == "main":
 
 # 1. detect when trash is inserted
 # 2. scan the trash
-# 3. rotate to bin
+# 3. rotate to bin, set ultrasonic's bin to corresponding bin
 # 4. open trapdoor
 # 5. measure bin capacity
 # 6. close trapdoor
