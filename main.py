f901a2941b6bf76bc7e6fa1b87c425e7aec74a6a
Kousuke Todd <mrkotodd@gmail.com>
2025-04-07 13:01:20 -0500

    made skeleton master file

---
+++ main.py
@@ -0,0 +1,30 @@
+# import statements
+
+def dist():
+    distance = 0
+    return dist
+
+def rotate():
+    a = 0
+
+def trapdoor():
+    a = 0
+
+def scanImg():
+    classification = ""
+    return classification
+
+if __name__ == "main":
+    
+
+
+# wstwz step by step
+
+# 1. detect when trash is inserted
+# 2. scan the trash
+# 3. rotate to bin
+# 4. open trapdoor
+# 5. measure bin capacity
+# 6. close trapdoor
+# 7. rotate back to original position
+# back to step 1
